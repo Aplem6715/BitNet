5f3383dd964f23d28419b5599abf51438dc0894a
Aplem6715 <Aplem6715@gmail.com>
2021-11-19 14:12:42 +0900

    intDense層の学習実装

--- src/layers/int/int_dense.h
+++ src/layers/int/int_dense.h
@@ -1,10 +1,25 @@
-﻿#ifndef INT_DENSE_H_INCLUDED_
+﻿/**
+ * @file int_dense.h
+ * @author Daichi Sato
+ * @brief Int型全結合層の定義
+ * @version 0.1
+ * @date 2021-11-19
+ * 
+ * @copyright Copyright (c) 2021 Daichi Sato
+ * 
+ * すべての層は次の層内にメンバ変数として展開される。
+ * テンプレートによって次元数などを決定するため，メモリ確保は行わず高速動作する。
+ * また，テンプレートによる展開を行うため，ヘッダー内にすべての関数を定義している。
+ * 
+ */
+
+#ifndef INT_DENSE_H_INCLUDED_
 #define INT_DENSE_H_INCLUDED_
 
 #include "../../net_common.h"
 
 /**
- * @brief Int型全結合層。-1/1のウェイトを持つ
+ * @brief Int型全結合層。-1/1のウェイトを持つ。
  * 
  * @tparam PreviousLayer_t 前のレイヤー型
  * @tparam OutputBits 次元数（ニューロン数）
@@ -52,17 +67,88 @@ public:
 
 	void ResetWeight()
 	{
-		// TODO
+		for (int i_out = 0; i_out < COMPRESS_IN_DIM; i_out++)
+		{
+			_bias[i_out] = 0;
+			for (int i_in = 0; i_in < COMPRESS_IN_DIM; i_in++)
+			{
+				double rand = GetRandReal() * 2 - 1;
+				_realWeight[i_out][i_in] = rand;
+				_weight[i_out][i_in] = rand > 0 ? 1 : -1;
+			}
+		}
+		_prev_layer.ResetWeight();
 	}
 
 #pragma region Train
 	const int *TrainForward(const int8_t *netInput)
 	{
-		// TODO
+		_inputBatchBuffer = _prev_layer.TrainForward(netInput);
+
+		for (int b = 0; b < BATCH_SIZE; b++)
+		{
+			int batchShiftIn = b * COMPRESS_IN_DIM;
+			int batchShiftOut = b * COMPRESS_OUT_DIM;
+			for (int i_out = 0; i_out < COMPRESS_OUT_DIM; i_out++)
+			{
+				// パディング分も含めて±1積和演算
+				int sum = _bias[i_out];
+				for (int i_in = 0; i_in < COMPRESS_IN_DIM; i_in++)
+				{
+					sum += _inputBatchBuffer[batchShiftIn + i_in] * _weight[i_out][i_in];
+				}
+
+				_outputBatchBuffer[batchShiftOut + i_out] = sum;
+			}
+		}
+
+		return _outputBatchBuffer;
 	}
 
 	void TrainBackward(const GradientType *nextGrad){
-		// TODO
+		// 勾配更新
+		for (int b = 0; b < BATCH_SIZE; b++)
+		{
+			int batchShiftIn = b * COMPRESS_IN_DIM;
+			int batchShiftOut = b * COMPRESS_OUT_DIM;
+			for (int i_in = 0; i_in < COMPRESS_IN_DIM; i_in++)
+			{
+				double sum = 0;
+				for (int i_out = 0; i_out < COMPRESS_OUT_DIM; i_out++)
+				{
+					sum += nextGrad[batchShiftOut + i_out] * _weight[i_out][i_in];
+				}
+				_grads[batchShiftIn + i_in] = sum;
+			}
+		}
+
+		for (int b = 0; b < BATCH_SIZE; b++)
+		{
+			int batchShiftIn = b * COMPRESS_IN_DIM;
+			int batchShiftOut = b * COMPRESS_OUT_DIM;
+			// 重み調整
+			for (int i_out = 0; i_out < COMPRESS_OUT_DIM; i_out++)
+			{
+				_bias[i_out] += nextLayerGrads[batchShiftOut + i_out];
+				for (int i_in = 0; i_in < COMPRESS_IN_DIM; i_in++)
+				{
+					_realWeight[i_out][i_in] += nextGrad[batchShiftOut + i_out] * _inputBatchBuffer[batchShiftIn + i_in];
+				}
+			}
+		}
+
+		// 2値化
+		for (int i_out = 0; i_out < COMPRESS_OUT_DIM; i_out++)
+		{
+			for (int i_in = 0; i_in < COMPRESS_IN_DIM; i_in++)
+			{
+				// Clipping
+				double tmp_w = std::max(-1.0, std::min(1.0, _realWeight[i_out][i_in]));
+				_realWeight[i_out][i_in] = tmp_w;
+
+				_weight[i_out][i_in] = (tmp_w > 0) ? 1 : -1;
+			}
+		}
 	}
 #pragma endregion
 };
